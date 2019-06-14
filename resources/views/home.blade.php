a6cb19ef5efaad2deab3b32670c579810ff84a81
Tyler Rister <tyler.rister@duethealth.com>
2019-06-14 14:50:49 -0400

    Fixed bug with skills always showing full

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -114,6 +114,24 @@
     <script type="text/javascript">
         $(document).ready(function() {
             GitHubActivity.feed({ username: "{{$profileAttributes['github_username']}}", selector: "#ghfeed" });
+            $('.level-bar-inner').css('width', '0');
+
+            $(window).on('load', function() {
+
+                $('.level-bar-inner').each(function() {
+
+                    var itemWidth = $(this).data('level');
+
+                    $(this).animate({
+                        width: itemWidth
+                    }, 800);
+
+                });
+
+            });
+
+            /* Bootstrap Tooltip for Skillset */
+            $('.level-label').tooltip();
         });
     </script>
 @endsection
